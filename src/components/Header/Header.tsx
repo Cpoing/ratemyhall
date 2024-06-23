ba41a16e5a29feea40cc9d5159f73a4e9f8090fb
Ted Lee <tedlee000@hotmail.com>
2024-06-23 18:50:47 -0400

    Added: Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">Rate my Hall</Link>
+      </div>
+      <nav className="nav">
+        <Link to="/about">About</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign up</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
