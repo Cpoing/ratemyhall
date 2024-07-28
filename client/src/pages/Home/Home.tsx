20cb0d70dd2cba531c0afc9f2f593d7a5b5885ff
Ted Lee <tedlee000@hotmail.com>
2024-07-27 22:02:21 -0400

    Testing urls

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -11,7 +11,7 @@ const Home: React.FC = () => {
     const fetchBuildings = async () => {
       try {
         const response = await fetch(`${backendUrl}/api/lecture-halls`);
-        console.log(backendUrl);
+        console.log(response);
         const data = await response.json();
         setBuildings(data.slice(0, 5));
       } catch (error) {
